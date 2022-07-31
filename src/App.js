ad3a0c545419498059fa5388d5c37a902208b580
Andrija2504 <andrijapavicevic000@gmail.com>
2022-07-31 12:12:41 +0200

    final commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import React, {useState, useEffect} from 'react'; 
 import Box from './Box.js';
- 
 
 function App() {
 
@@ -10,17 +9,19 @@ function App() {
   const [lastName, setLastName] = useState(null);
   const [street, setStreet] = useState(null);
   const [city, setCity] = useState(null);
+  const [number, setNumber] = useState(0);
 
   const [list, setList] = useState([]);
 
-  const handleClick = (e) => {
-    e.preventDefault();
+  const handleClick = () => {
     if(firstName == null || lastName == null || street == null || city == null
       || firstName === '' || lastName === '' || street === '' || city === ''){
       alert('Fill all data');
     }
     else {
+      setNumber(number+1);
       const listObject = [{
+        'number': number,
         'firstName': firstName,
         'lastName': lastName,
         'street': street,
@@ -36,44 +37,45 @@ function App() {
   }
 
   return (
-    <div>
-      <form className='forma'>
-        <div className='formDiv'>
-          <label for="firstName" id="firstName">Firstname</label> 
-          <input type="text" id="firstName" name="firstName" placeholder='Firstname' 
-          onChange={(event) => {
-            setFirstName(event.target.value);
-          }}/>
-        </div>
-        <div className='formDiv'>
-          <label for="lastName" id="lastName">Lastname</label> 
-          <input type="text" id="lastName" name="lastName" placeholder='Lastname' 
-          onChange={(event) => {
-            setLastName(event.target.value);
-          }}/>
-        </div>
-        <div className='formDiv'>
-          <label for="street" id="street">Street</label> 
-          <input type="text" id="street" name="street" placeholder='Street' style={{width:'250px'}} 
-          onChange={(event) => {
-            setStreet(event.target.value);
-          }}/>
-        </div>
-        <div className='formDiv'>
-          <label for="city" id="city">City</label> 
-          <input type="text" id="city" name="city" placeholder='City' style={{width:'250px'}} 
-          onChange={(event) => {
-            setCity(event.target.value);
-          }}/>
-        </div>
-        <div className='formDiv' style={{alignSelf: 'flex-end', width: '15%'}}>
-          <button className='btn btn-primary' onClick={handleClick}>Insert</button>
-        </div>
-      </form>
-
+    <div style={{backgroundColor: '#A9A9A9', padding: '10px'}}>
+      <div className='divClass' style={{backgroundColor: 'white'}}>
+        <form className='forma'>
+          <div className='formDiv'>
+            <label for="firstName" id="firstName">Firstname</label> 
+            <input type="text" id="firstName" name="firstName" placeholder='Firstname' 
+            onChange={(event) => {
+              setFirstName(event.target.value);
+            }}/>
+          </div>
+          <div className='formDiv'>
+            <label for="lastName" id="lastName">Lastname</label> 
+            <input type="text" id="lastName" name="lastName" placeholder='Lastname' 
+            onChange={(event) => {
+              setLastName(event.target.value);
+            }}/>
+          </div>
+          <div className='formDiv'>
+            <label for="street" id="street">Street</label> 
+            <input type="text" id="street" name="street" placeholder='Street' style={{width:'250px'}} 
+            onChange={(event) => {
+              setStreet(event.target.value);
+            }}/>
+          </div>
+          <div className='formDiv'>
+            <label for="city" id="city">City</label> 
+            <input type="text" id="city" name="city" placeholder='City' style={{width:'250px'}} 
+            onChange={(event) => {
+              setCity(event.target.value);
+            }}/>
+          </div>
+          <div className='formDiv' style={{alignSelf: 'flex-end', width: '15%'}}>
+            <button type='button' className='btn btn-primary' onClick={handleClick}>Insert</button>
+          </div>
+        </form>
+      </div>
     {list.map((val, key) => {
       return(
-        <Box item={val}/>
+        <Box item={val} list={list} setList={setList}/>
       )
     })}
     
--- src/Box.js
+++ src/Box.js
@@ -2,15 +2,22 @@ import logo from './logo.svg';
 import './Box.css';
 import React, {useState, useEffect} from 'react'; 
 
-function Box(props) {
-    let val = props.item;
+function Box({item, list, setList}) {
+    let val = item;
 
-    const removeButton = (e) => {
-        e.preventDefault();
-        alert('izbrisa ga')
+    const removeButton = (e, numberToDelete) => {
+        setList(current =>
+        current.filter(list => {
+            return list.number !== numberToDelete;
+        }));
+
+        console.log(list);
+
+        alert('asdasdas');
+
     }
     return (
-        <div>
+        <div className='divClass' style={{backgroundColor: 'white'}}>
       <form className='forma'>
         <div className='formDiv'>
             {val.firstName}
@@ -25,7 +32,7 @@ function Box(props) {
             {val.city}
         </div>
         <div className='formDiv' style={{alignSelf: 'flex-end', width: '15%'}}>
-          <button className='btn btn-primary' onClick={removeButton}>Remove</button>
+          <button type='button' className='btn btn-primary' onClick={() => removeButton(val.number)}>Remove</button>
         </div>
       </form>
     </div>
